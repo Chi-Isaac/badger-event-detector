a6265832dca1b8d23f0b84a0c762acf318b0cc3d
Isaac Chi <isaacchi0707@gmail.com>
2026-07-12 23:17:35 +0100

    comments explaining args

--- src/inference.py
+++ src/inference.py
@@ -23,12 +23,12 @@ def parse_args():
     parser.add_argument("--model", type=Path, required=True, help="Path to model weights (.pt file)")
     parser.add_argument("--source", type=Path, required=True, help="Path to input video")
     parser.add_argument("--outdir", type=Path, required=True, help="Path to output directory")
-    parser.add_argument("--conf", type=float, default=0.3)
-    parser.add_argument("--iou", type=float, default=0.7)
-    parser.add_argument("--imgsz", type=int, default=640)
-    parser.add_argument("--device", type=str, default="0")
-    parser.add_argument("--stride", type=int, default=1)
-    parser.add_argument("--save", action="save_true")
+    parser.add_argument("--conf", type=float, default=0.3) # Confidence threshold for detections
+    parser.add_argument("--iou", type=float, default=0.7) # Intersection over Union threshold (IoU = Area of Overlap / Area of Union)
+    parser.add_argument("--imgsz", type=int, default=640) # Inference image size (pixels)
+    parser.add_argument("--device", type=str, default="0") # Device to run inference on ("cpu" or "0" for GPU)
+    parser.add_argument("--stride", type=int, default=1) # Stride for fram sampling
+    parser.add_argument("--save", action="save_true") # Save inference video with bounding boxes drawn
     
     return parser.parse_args()
 
