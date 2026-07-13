709ac0a49ccd9faeab2cab3203cf96b62b60062b
Isaac Chi <isaacchi0707@gmail.com>
2026-07-13 23:33:53 +0100

    group detections by frame

--- src/track.py
+++ src/track.py
@@ -1,3 +1,5 @@
+import json
+
 class Track:
     track_id: str
     last_xyxy: list[float]
@@ -5,3 +7,14 @@ class Track:
     hits: int
     misses: int
     state: str
+
+# Takes jsonl with all detections and returns list of detections by frame
+def load_detections(path):
+    detections_by_frame = []
+    
+    with open(path, "r") as f:
+        for line in f:
+            record = json.loads(line)
+            detections_by_frame[record["frame_index"]].append(record)
+    
+    return detections_by_frame
