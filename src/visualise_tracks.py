0c870b74496fca24b3ebd789b160faeaca50cffc
Isaac Chi <isaacchi0707@gmail.com>
2026-08-24 21:49:24 +0100

    track box colour generation

--- src/visualise_tracks.py
+++ src/visualise_tracks.py
@@ -8,6 +8,22 @@ def parse_args():
     parser.add_argument("--output", type=Path, required=True, help="Path to output annotated video")
     return parser.parse_args()
 
+# Uses track ID as a seed to randomly generate a consistent hue.
+# Colour converted from HSV space to RGB
+def track_colour(track_id):
+    rng = random.Random(track_id)
+    hue = rng.random()
+    saturation = 0.75
+    value = 0.90
+    
+    r, g, b = colorsys.hsv_to_rgb(hue, saturation, value)
+    
+    return (
+        round(r * 255),
+        round(g * 255),
+        round(b * 255),
+    )
+
 def main():
     args = parse_args()
 
